f615e91e833d1ff036fe800f8ca9f00843fd6548
JulienPetit <julienpetit1@gmail.com>
2021-12-09 18:38:34 +0100

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,45 @@
      <nav class="navbar navbar-expand navbar-dark bg-dark">
       <router-link to="/" class="navbar-brand">Groupomania</router-link>
       <div class="navbar-nav mr-auto">
+
+        <li class="nav-item">
+          <router-link to="/home" class="nav-link">
+            <font-awesome-icon icon="home" /> Home
+          </router-link>
+        </li>
+
+          <li class="nav-item">
+          <router-link to="/user" class="nav-link">User</router-link>
+        </li>
+      </div>
+
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+            <font-awesome-icon icon="user-plus" /> Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" /> Login
+          </router-link>
+        </li>
+      </div>
+
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            <font-awesome-icon icon="user" />
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logOut">
+            <font-awesome-icon icon="sign-out-alt" /> LogOut
+          </a>
+        </li>
+      </div>
+       <div>
         <li class="nav-item">
           <router-link to="/posts" class="nav-link">Posts</router-link>
         </li>
@@ -12,7 +51,7 @@
       </div>
     </nav>
 
-    <div class="container mt-3">
+    <div class="container">
       <router-view />
     </div>
   </div>
